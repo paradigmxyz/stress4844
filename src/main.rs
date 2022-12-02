bf7fff91ef981cc9aafc5162a02658138cc84993
Georgios Konstantopoulos <me@gakonst.com>
2022-12-01 18:55:58 -0800

    feat: bundles are landing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use ethers_flashbots::{BundleRequest, FlashbotsMiddleware};
 
 // CLI
 use clap::Parser;
+use eyre::Result;
 use tracing_subscriber::{filter::EnvFilter, prelude::*};
 
 // Misc
 use rand::{distributions::Standard, Rng};
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 use url::Url;
 
 #[derive(Debug, Parser)]
@@ -39,6 +40,13 @@ struct Opts {
 
     #[arg(default_value = "100", long, short)]
     gas_price: U256,
+
+    #[arg(long, short, value_parser = from_dec_str)]
+    payment: U256,
+}
+
+fn from_dec_str(s: &str) -> Result<U256, String> {
+    U256::from_dec_str(s).map_err(|e| e.to_string())
 }
 
 fn http_provider(s: &str) -> Result<String, String> {
@@ -69,16 +77,27 @@ const KB: usize = 1024;
 /// transaction
 const TRIM_BYTES: usize = 500;
 
+/// Address of the following contract to allow for easy coinbase payments on Goerli.
+///
+/// contract CoinbasePayer {
+///     receive() payable external {
+///         payable(address(block.coinbase)).transfer(msg.value);
+///     }
+/// }
+const COINBASE_PAYER_ADDR: &str = "0x060d6635bb76c71871f97C12f10Fa20BD8e87eC0";
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let opts = Opts::parse();
+    let payment = opts.payment;
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(EnvFilter::new("stress4844=trace"))
         .init();
 
-    let provider = Arc::new(Provider::try_from(opts.rpc_url)?);
+    let interval = Duration::from_secs(1);
+    let provider = Arc::new(Provider::try_from(opts.rpc_url)?.interval(interval));
     let signer = opts
         .tx_signer
         .strip_prefix("0x")
@@ -93,7 +112,7 @@ async fn main() -> eyre::Result<()> {
 
     let bundle_middleware = FlashbotsMiddleware::new(
         provider.clone(),
-        Url::parse("https://boost-relay-goerli.flashbots.net/")?,
+        Url::parse("https://relay-goerli.flashbots.net/")?,
         bundle_signer,
     );
 
@@ -103,18 +122,20 @@ async fn main() -> eyre::Result<()> {
         .get_block(BlockNumber::Latest)
         .await?
         .expect("could not get latest block");
-    let mut nonce = provider
+    let nonce = provider
         .get_transaction_count(address, Some(BlockNumber::Pending.into()))
         .await?;
 
     tracing::info!(
         "starting benchmark from {:?} (balance: {} ETH, nonce: {})",
         address,
-        ethers::core::utils::format_units(balance, "gwei")?,
+        ethers::core::utils::format_units(balance, "eth")?,
         nonce
     );
+    tracing::info!("builder payment {}", payment);
     tracing::debug!("block gas limit: {} gas", block.gas_limit);
-    let provider = SignerMiddleware::new_with_provider_chain(bundle_middleware, signer).await?;
+    let provider =
+        Arc::new(SignerMiddleware::new_with_provider_chain(bundle_middleware, signer).await?);
     let chain_id = provider.signer().chain_id();
 
     // TODO: Do we want this to be different per transaction?
@@ -127,10 +148,8 @@ async fn main() -> eyre::Result<()> {
     // Sample junk data for the blob.
     let blob = rand::thread_rng()
         .sample_iter(Standard)
-        // .take(chunks)
-        .take(10) // TODO: Remove.
+        .take(1024)
         .collect::<Vec<u8>>();
-    let blob_len = blob.len();
 
     // Craft the transaction.
     let tx = TransactionRequest::new()
@@ -141,16 +160,82 @@ async fn main() -> eyre::Result<()> {
         .data(blob)
         .gas_price(opts.gas_price);
 
+    let mut bundle = construct_bundle(
+        provider.clone(),
+        &tx,
+        block.gas_limit,
+        opts.fill_pct,
+        nonce,
+        payment,
+    )
+    .await?;
+
+    // on every block try to get the bundle in
+    let mut block_sub = provider.watch_blocks().await?;
+    tracing::info!("subscribed to blocks - waiting for next");
+    while block_sub.next().await.is_some() {
+        let block_number = provider.get_block_number().await?;
+
+        let span = tracing::trace_span!("submit-bundle", block = block_number.as_u64());
+        let _enter = span.enter();
+
+        bundle = bundle
+            .set_block(block_number + 1)
+            .set_simulation_block(block_number)
+            .set_simulation_timestamp(0);
+        tracing::debug!("bundle target block {:?}", block_number + 1);
+
+        let pending_bundle = provider.inner().send_bundle(&bundle).await?;
+        match pending_bundle.await {
+            Ok(bundle_hash) => {
+                // TODO: Can we log more info from the Flashbots API?
+                tracing::info!("bundle included! hash: {:?}", bundle_hash);
+                let nonce = provider
+                    .get_transaction_count(address, Some(BlockNumber::Pending.into()))
+                    .await?;
+                tracing::debug!("signing new bundle for next block (new nonce: {})", nonce);
+                bundle = construct_bundle(
+                    provider.clone(),
+                    &tx,
+                    block.gas_limit,
+                    opts.fill_pct,
+                    nonce,
+                    payment,
+                )
+                .await?;
+            }
+            Err(err) => {
+                tracing::error!("{}. Retrying.", err);
+            }
+        }
+    }
+
+    tracing::debug!("Done! End Block: {}", provider.get_block_number().await?);
+
+    Ok(())
+}
+
+#[tracing::instrument(skip_all, name = "construct_bundle")]
+async fn construct_bundle<M: Middleware + 'static>(
+    provider: Arc<SignerMiddleware<M, LocalWallet>>,
+    tx: &TransactionRequest,
+    gas_limit: U256,
+    fill_pct: u8,
+    mut nonce: U256,
+    payment: U256,
+) -> Result<BundleRequest> {
     let gas_per_tx = provider.estimate_gas(&tx.clone().into(), None).await?;
     tracing::debug!("tx cost {} gas", gas_per_tx);
 
     // For each block, we want `fill_pct` -> we generate N transactions to reach that.
-    let gas_used_per_block = block.gas_limit * opts.fill_pct / 100;
-    let txs_per_block = 2; //  (gas_used_per_block / gas_per_tx).as_u64();
-    tracing::info!(
-        "submitting {txs_per_block} {blob_len} KB txs per block for {} blocks",
-        opts.blocks
-    );
+    let gas_used_per_block = gas_limit * fill_pct / 100;
+
+    // let txs_per_block = (gas_used_per_block / gas_per_tx).as_u64();
+    let txs_per_block = 10; //  (gas_used_per_block / gas_per_tx).as_u64();
+    let blob_len = tx.data.as_ref().map(|x| x.len()).unwrap_or_default();
+    tracing::debug!("submitting {txs_per_block} {blob_len} KB txs per block",);
+
+    let gas_price = provider.get_gas_price().await?;
 
     // Construct the bundle
     let mut bundle = BundleRequest::new();
@@ -158,8 +243,8 @@ async fn main() -> eyre::Result<()> {
         let mut tx = tx.clone();
 
         // increment the nonce and apply it
-        nonce += 1.into();
         tx.nonce = Some(nonce);
+        nonce += 1.into();
         tx.gas = Some(gas_per_tx);
 
         // make into typed tx for the signer
@@ -169,27 +254,18 @@ async fn main() -> eyre::Result<()> {
         bundle = bundle.push_transaction(rlp);
     }
 
-    let tx = TransactionRequest::new()
-        .to("0x0000000000000000000000000000000000000000".parse::<Address>()?)
-        .value(100)
-        .into();
-    let signature = provider.signer().sign_transaction(&tx).await?;
-    let rlp = tx.rlp_signed(&signature);
-    bundle = bundle.push_transaction(rlp);
+    tracing::debug!("signed {} transactions", txs_per_block);
 
-    tracing::info!("signed {} transactions", txs_per_block);
+    // let payment = TransactionRequest::new()
+    //     .to(COINBASE_PAYER_ADDR.parse::<Address>()?)
+    //     .nonce(nonce)
+    //     .gas(30000)
+    //     .gas_price(gas_price)
+    //     .value(payment)
+    //     .into();
+    // let signature = provider.signer().sign_transaction(&payment).await?;
+    // let rlp = tx.rlp_signed(&signature);
+    // bundle = bundle.push_transaction(rlp);
 
-    // configure the bundle to fire it away
-    let block_number = provider.get_block_number().await?;
-    bundle = bundle
-        .set_block(block_number + 1)
-        .set_simulation_block(block_number)
-        .set_simulation_timestamp(0);
-
-    let simulated_bundle = provider.inner().simulate_bundle(&bundle).await?;
-    tracing::info!("simulated {:?}", simulated_bundle);
-
-    tracing::debug!("Done! End Block: {}", provider.get_block_number().await?);
-
-    Ok(())
+    Ok(bundle)
 }
